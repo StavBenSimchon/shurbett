4f1e26e67dbf59688980367b7648bca94fd096ec
asdf <a@a.a>
2020-09-15 16:17:32 +0300

    a

--- python_spider/init.sh
+++
@@ -1,5 +0,0 @@
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" > /etc/apk/repositories
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
-apk update
-apk add chromium chromium-chromedriver
-pip install -U selenium
