59fd4e9f0584a9367e8cfe7c9ff2f6e811512b77
asdf <a@a.a>
2020-09-15 14:00:48 +0300

    a

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException#Install Driver
 option = webdriver.ChromeOptions()
 option.add_argument(" — incognito")
-browser = webdriver.Chrome(options=option)
+# browser = webdriver.Chrome(options=option)
+browser = webdriver.Chrome()
 browser.get("https://www.winner.co.il/mainbook/sport-%D7%9B%D7%93%D7%95%D7%A8%D7%92%D7%9C/ep-%D7%9E%D7%95%D7%A2%D7%93%D7%95%D7%A0%D7%99%D7%9D-%D7%91%D7%99%D7%A0%D7%9C%D7%90%D7%95%D7%9E%D7%99%D7%99%D7%9D/ep-%D7%9E%D7%95%D7%A7%D7%93%D7%9E%D7%95%D7%AA-%D7%9C%D7%99%D7%92%D7%AA-%D7%94%D7%90%D7%9C%D7%95%D7%A4%D7%95%D7%AA/event-%D7%93%D7%99%D7%A0%D7%9E%D7%95-%D7%A7%D7%99%D7%99%D7%91---%D7%90%D7%9C%D7%A7%D7%9E%D7%90%D7%A8")
