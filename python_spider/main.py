2a86c6d99884cbf6e87fe2fdf50aebe3f46fc531
asdf <a@a.a>
2020-09-17 02:07:33 +0300

    p

--- python_spider/main.py
+++ python_spider/main.py
@@ -1,7 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://www.winner.co.il/mainbook/sport-%D7%9B%D7%93%D7%95%D7%A8%D7%92%D7%9C/ep-%D7%9E%D7%95%D7%A2%D7%93%D7%95%D7%A0%D7%99%D7%9D-%D7%91%D7%99%D7%A0%D7%9C%D7%90%D7%95%D7%9E%D7%99%D7%99%D7%9D/ep-%D7%9E%D7%95%D7%A7%D7%93%D7%9E%D7%95%D7%AA-%D7%9C%D7%99%D7%92%D7%AA-%D7%94%D7%90%D7%9C%D7%95%D7%A4%D7%95%D7%AA/event-%D7%93%D7%99%D7%A0%D7%9E%D7%95-%D7%A7%D7%99%D7%99%D7%91---%D7%90%D7%9C%D7%A7%D7%9E%D7%90%D7%A8')
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+from pprint import pprint
+# create an HTML Session object
+session = HTMLSession()
+# Use the object above to connect to needed webpage
+resp = session.get("https://finance.yahoo.com/quote/NFLX/options?p=NFLX")
+# Run JavaScript code on webpage
+import os.path 
+if os.path.isfile('/root/.local/share/pyppeteer/local-chromium/588429/chrome-linux/chrome'):
+    print("exists")
+else:
+    print("not exists")
+s = resp.html.render()
+soup = BeautifulSoup(resp.html.html, "lxml")
+breakpoint()
+pprint(s)
+pprint(soup)
