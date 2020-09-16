8d214eee05e5793075c5ec2be781346595ba13d1
idanK <idankallmann91@gmail.com>
2020-09-16 22:13:52 +0300

    puppeter test

---
+++ nodeCrawler/spider/puppeter.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  try {
+    console.log("1");
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.goto('https://findair.co.uk', {
+      waitUntil: 'networkidle2',
+      timeout: 300000
+    });
+    await page.waitForSelector('.main', { timeout: 1000 });
+    // console.log(browser);
+    // console.log(page);
+
+    const body = await page.evaluate(() => {
+      console.log("@@@@@@@@@@@@@");
+      return document.querySelector('#from').placeholder;
+    });
+    console.log(body);
+
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
+})();
