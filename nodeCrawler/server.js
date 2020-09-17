9d4a730f205379686a27b615dea1ae592fd5b88c
idanK <idankallmann91@gmail.com>
2020-09-17 15:41:13 +0300

    check puppeteer

--- nodeCrawler/server.js
+++ nodeCrawler/server.js
@@ -1,11 +1,11 @@
 const http = require('http')
 const server = http.createServer();
-// const puppeteer = require('./spider/puppeter');
+const puppeteer = require('./spider/puppeter');
 
-const Spider = require('./spider/spider')
+// const Spider = require('./spider/spider')
 
 // console.log(Spider);
-Spider()
+// Spider()
 server.listen(8000);
 
 console.log("Listen on port 8000");
