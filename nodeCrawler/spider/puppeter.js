12c66e6b8637e50248ea69f8afef1fa0adaad9ec
idanK <idankallmann91@gmail.com>
2020-09-21 20:29:29 +0300

    xxx

--- nodeCrawler/spider/puppeter.js
+++ nodeCrawler/spider/puppeter.js
@@ -6,24 +6,29 @@ const config = require('../config.json');
 (async () => {
   try {
     const browser = await puppeteer.launch({
-      headless: true,
-      executablePath: '/usr/bin/chromium-browser',
-      args: ['--no-sandbox', '--headless', '--disable-gpu']
+      headless: false,
+      // executablePath: '/usr/bin/chromium-browser',
+      // args: ['--no-sandbox', '--headless', '--disable-gpu']
     });
     const page = await browser.newPage();
 
     async function page_counter_line_events() {
+      console.log("START CRAWLING >>>>>>>>");
       const counterList = new Map()
       let winner = config['winner'].urls, i = 0
       for (i = 0; i < winner.length; i++) {
+        console.log(`URL >>>> ${config['winner'].baseUrl}${winner[i]}`);
         await page.goto(`${config['winner'].baseUrl}${winner[i]}`, {
-          waitUntil: 'networkidle0'
+          waitUntil: 'networkidle2'
         })
 
         const count = await page.$$eval('.market_count > a', e=>e.map(a => a.href))
+        console.log(`${i} >>>>>>> ${count}`);
         count.map(c => {
           let linkSplit = c.split('/')
           const key = linkSplit[linkSplit.length - 1]
+          console.log(`LIST NUMBER ${i}`);
+          console.log(`${key} >>>> ${c}`);
           counterList.set(key, c)
         })
       }
@@ -32,7 +37,7 @@ const config = require('../config.json');
     }
 
     await page_counter_line_events()
-
+    console.log("<<<<<<<<<<<<< CLOSING BROWSER >>>>>>>>>>>>>");
     await browser.close();
   } catch (error) {
     console.log('error - ', error);
